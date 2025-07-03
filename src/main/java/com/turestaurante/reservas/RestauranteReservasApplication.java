38bd73fe054f07b1b0648594cf5e42e1aba8103d
GuilmarCJ <cancan22joaquin@gmail.com>
2025-07-03 14:34:48 -0500

    Solucion para zure

--- src/main/java/com/turestaurante/reservas/RestauranteReservasApplication.java
+++ src/main/java/com/turestaurante/reservas/RestauranteReservasApplication.java
@@ -5,7 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestauranteReservasApplication {
+
     public static void main(String[] args) {
+        // Usa el puerto que Azure proporciona
+        String port = System.getenv("PORT");
+        if (port != null) {
+            System.setProperty("server.port", port);
+        }
+
         SpringApplication.run(RestauranteReservasApplication.class, args);
     }
 }
